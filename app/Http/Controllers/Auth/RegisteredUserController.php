adff335dd6f4e4bd237afe0798450fa3cb499c6a
Cletus Kingdom <cletuskingdom@gmail.com>
2023-09-18 18:38:57 +0100

    added the endpoint for registration

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
+use Illuminate\Support\Facades\Validator;
 
 class RegisteredUserController extends Controller
 {
@@ -18,24 +19,34 @@ class RegisteredUserController extends Controller
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function store(Request $request): Response
+    public function store(Request $request)
     {
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
+      
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+        $token = $user->createToken('register_token')->plainTextToken;
 
         event(new Registered($user));
-
-        Auth::login($user);
-
-        return response()->noContent();
+        $response = [
+            'response_code' => 201,
+            'response_message' => 'User created successfuly',
+            'data' => [
+                'token' => $token,
+                'user' => $user,
+            ]
+        ];
+        return response()->json($response, 201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+// unprotected route, for users that aren't logged in
+Route::middleware(['guest'])->group(function () {
+    Route::post('v1/register', [RegisteredUserController::class, 'store'])->name('register'); // create a user
+});
+
+Route::group(['prefix' => 'v1', 'middleware' => ['auth:sanctum']], function() {
+    Route::get('/users', [UserController::class, 'index']); // get a list of users
 });
